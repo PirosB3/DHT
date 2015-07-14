c0b6b0c22e01242ff9b50aa7e68593b498ab85e4
Daniel Pyrathon <pirosb3@gmail.com>
2015-07-14 17:05:02 +0200

    added node and table

---
+++ node.py
@@ -0,0 +1,57 @@
+import itertools
+import random
+import unittest
+
+
+def random_20bits():
+    return bytearray([random.randint(0, 255) for _ in xrange(20)])
+
+class Node(object):
+    def __init__(self, data):
+        assert len(data) == 20
+        self.data = data
+
+    def __xor__(self, other):
+        return Node(bytearray([bit_a ^ bit_b for bit_a, bit_b in zip(self.data, other.data)]))
+
+    def __eq__(self, other):
+        return other.data == self.data
+
+    def distance_key(self):
+        res = 0 
+        for bit_n, byte in enumerate(self.data):
+            for i in xrange(7, -1, -1):
+                if byte & (1 << i) != 0:
+                    return bit_n * 8 + (7 - i)
+
+        return len(self.data) * 8
+
+
+class NodeTestCase(unittest.TestCase):
+
+    def test_it_exists(self):
+        n = Node(bytearray(['a'] * 20))
+        self.assertEqual(list(n.data), list(itertools.repeat(97, 20)))
+
+    def test_random_node_creation_works(self):
+        n = Node(random_20bits())
+        self.assertEqual(len(n.data), 20)
+
+    def test_xor_distance(self):
+        n1 = Node(bytearray(['a'] * 20))
+        n2 = Node(bytearray(['b'] * 20))
+        result = n1 ^ n2
+        self.assertEqual(list(result.data), list(itertools.repeat(3, 20)))
+
+    def test_distance_key(self):
+        payload = [0] * 5 + [(2**5)-1] + [255] * 14
+        n = Node(bytearray(payload))
+        self.assertEqual(n.distance_key(), (8 * 5) + 3)
+
+        payload = [0] * 20
+        n = Node(bytearray(payload))
+        self.assertEqual(n.distance_key(), 20 * 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ table.py
@@ -0,0 +1,77 @@
+import itertools
+import random
+import unittest
+from collections import deque
+
+from node import Node, random_20bits
+
+
+class RoutingTable(object):
+    def __init__(self, node):
+        self.node = node
+        self.buckets = [deque() for _ in xrange(20 * 8)]
+
+    def update(self, node):
+        bucket_n = (self.node ^ node).distance_key()
+        if node not in self.buckets[bucket_n]:
+            self.buckets[bucket_n].append(node)
+
+    def _find_closest_bucket(self, starting_bucket_idx, target_node, n=20):
+        shift = 0
+        result = []
+        finished_left = finished_right = False
+        while finished_left == False or finished_right == False:
+            if len(result) >= n:
+                break
+            # Handle left
+            left_idx = starting_bucket_idx - shift
+            finished_left = left_idx < 0
+            if not finished_left:
+                result.extend([
+                    ((target_node ^ n).distance_key(), n)
+                    for n in self.buckets[left_idx]
+                ])
+
+            right_idx = starting_bucket_idx + shift
+            finished_right = right_idx >= len(self.buckets)
+            if not finished_right and shift > 0:
+                result.extend([
+                    ((target_node ^ n).distance_key(), n)
+                    for n in self.buckets[right_idx]
+                ])
+            shift += 1
+        
+        return sorted(result)
+
+
+class TableTestCase(unittest.TestCase):
+    def setUp(self):
+        self.node = Node(bytearray(['a'] * 20))
+
+    def test_table_exists(self):
+        table = RoutingTable(self.node)
+        self.assertTrue(table)
+        self.assertEqual(len(table.buckets), 20 * 8)
+
+    def test_update_node(self):
+        table = RoutingTable(self.node)
+        table.update(Node(bytearray(['b'] * 20)))
+        self.assertEqual(len(table.buckets[6]), 1)
+
+    def test_find_closest(self):
+        nodes = [Node(random_20bits()) for _ in xrange(6)]
+        table = RoutingTable(self.node)
+        table.buckets[4] = nodes[:2]
+        table.buckets[3] = [nodes[2]]
+        table.buckets[5] = nodes[3:5]
+        table.buckets[10] = [nodes[5]]
+
+        search_node = Node(random_20bits())
+        result = table._find_closest_bucket(4, search_node)
+        self.assertEqual(result, sorted([
+            ((search_node ^ n).distance_key(), n) for n in nodes
+        ]))
+
+
+if __name__ == '__main__':
+    unittest.main()
