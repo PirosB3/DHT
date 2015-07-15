e3d5ad989923b7d95d469b20b1b135c51172246a
Daniel Pyrathon <pirosb3@gmail.com>
2015-07-15 18:20:26 +0200

    added threading

--- dht.py
+++ dht.py
@@ -1,6 +1,10 @@
+import random
+import sys
 import json
 import zmq
+from itertools import count
 
+import threading
 from node import Node, random_20bits
 from table import RoutingTable
 
@@ -12,6 +16,11 @@ class DHT(object):
         self.routing_table = RoutingTable(node, bootstrap)
         self.context = zmq.Context()
         self._sockets = {}
+        self._thread_uid = None
+
+    @property
+    def started(self):
+        return self._thread_uid != None
 
     def find_node_handler(self, key, sock):
         response = []
@@ -39,8 +48,10 @@ class DHT(object):
 
     def iterative_find_node(self, key):
         seen = set()
-        for _ in xrange(3):
-            for _, node in self.routing_table.find_closest(self.node):
+        while True:
+            closest_nodes = self.routing_table.find_closest(self.node)
+            processed = set()
+            for _, node in closest_nodes:
                 node_key = tuple(node.data)
                 if node_key not in seen:
                     new_nodes = self.send_message_to_node(node, 'FIND_NODE', tuple(key.data))
@@ -48,16 +59,35 @@ class DHT(object):
                         new_node = Node(bytearray(data), host, port)
                         if new_node != self.node:
                             print "Discovered %s" % new_node
+                            processed.add(tuple(new_node.data))
                             self.routing_table.update(new_node)
                     seen.add(node_key)
-        print self.routing_table.buckets
+
+            if len(seen) >= 20 or len(processed) == 0:
+                print "BREAKING"
+                break
+        print "FINISHED ITERATIVE FIND NODE FOR %s" % self.node
 
     def run(self):
-        socket = self.context.socket(zmq.REP)
-        socket.bind("tcp://*:%s" % self.node.port)
+        self._thread_uid = threading.Thread(target=self._run)
+        self._thread_uid.daemon = True
+        self._thread_uid.start()
         if self.bootstrap:
             self.iterative_find_node(self.node)
+        return self._thread_uid
+
+    def _run(self):
+        socket = self.context.socket(zmq.REP)
+        socket.bind("tcp://*:%s" % self.node.port)
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
         while True:
+            found = poller.poll(2000)
+            if len(found) == 0:
+                nodes_known = [len(b) for b in self.routing_table.buckets]
+                print "%s knows %s peers" % (self.node, sum(nodes_known))
+                continue
+
             message = json.loads(socket.recv())
 
             # Update routing table if message is from a new
@@ -67,11 +97,11 @@ class DHT(object):
                 message['from']['host'],
                 message['from']['port']
             )
-            print "Received from %s" % req_node
+            #print "Received from %s" % req_node
             self.routing_table.update(req_node)
             if message['type'] == 'FIND_NODE':
                 self.find_node_handler(message['value'], socket)
-                print self.routing_table.buckets
+                #print self.routing_table.buckets
 
 
 def run(uid, port, boot, boot_port):
@@ -80,7 +110,6 @@ def run(uid, port, boot, boot_port):
     if boot and boot_port:
         boot_node = Node(bytearray(boot), port=boot_port)
     dht = DHT(node, boot_node)
-    print list(node.data)
     dht.run()
 
 def slave(uid):
@@ -91,17 +120,22 @@ def slave(uid):
 
 
 if __name__ == '__main__':
-    m1 = [243, 103, 248, 223, 230, 45, 34, 177, 50, 180, 195, 195, 135, 146, 212, 158, 9, 55, 10, 94]
-    m2 = [243, 106, 248, 223, 230, 230, 34, 177, 50, 180, 195, 195, 135, 146, 212, 158, 9, 55, 10, 94]
-    m3 = [130, 106, 248, 223, 230, 230, 34, 177, 50, 180, 195, 195, 135, 146, 212, 158, 9, 55, 10, 94]
-    m4 = [130, 220, 248, 223, 230, 230, 34, 177, 50, 180, 195, 195, 135, 146, 212, 158, 9, 55, 10, 94]
+    ports = count(3000)
+    processes = []
+    kads = []
+    n_processes = int(sys.argv[1])
+    for _ in xrange(n_processes):
+        try:
+            seed = random.choice(kads).node
+        except IndexError:
+            seed = None
 
-    import sys
-    if sys.argv[1] == 'master':
-        run(m1, 3000, boot=None, boot_port=None)
-    elif sys.argv[1] == 'slave1':
-        run(m2, 3001, boot=m1, boot_port=3000)
-    elif sys.argv[1] == 'slave2':
-        run(m3, 3002, boot=m1, boot_port=3000)
-    elif sys.argv[1] == 'slave3':
-        run(m4, 3003, boot=m2, boot_port=3001)
+        node = Node(random_20bits(), port=next(ports))
+        dht = DHT(node, seed)
+        kads.append(dht)
+
+        #from time import sleep
+        #sleep(0.1)
+        processes.append(dht.run())
+
+    [p.join() for p in processes]
